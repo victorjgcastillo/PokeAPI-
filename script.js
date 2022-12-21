481b109fdc990937437ce61b0ce8847584b0060f
victorjgcastillo <od.victorgc@gmail.com>
2022-12-21 23:03:06 +0100

    agregar un offset y funcion add event listener, para que se reproduzcan 9 pokemons por pagina

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const pokemonContainer = document.querySelector(".pokemon_container");
 const spinner = document.querySelector("#spinner");
 //traemos de primero los elementos de html (el spinner y el pokemoncontainer)
+const previuos = document.querySelector('#previous');
+const next = document.querySelector('#next');
+
+let offset = 1;
+let limit = 8;
+
+previuos.addEventListener('click', () => {
+    if (offset != 1) {
+    offset -= 9;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset,limit);
+    }
+})
+
+next.addEventListener('click', () => {
+    offset += 9;
+    fetchPokemons(offset,limit);
+})
 
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -8,18 +26,29 @@ function fetchPokemon(id) {
     //.then (data => console.log(data)) "esta funcion sencilla fue para probar llevar a consola a solo el primer pokemon"
     .then((data) => {
         createPokemon(data);
+        //se coloca el spinner en la funcion de fetch pokemon escondido
         spinner.style.display = "none";
     })  
 }
 //fetchPokemon(1) "prueba de imprimir en mi consola los datos de mi primer pokemon"
 
-function fetchPokemons(number){
-    spinner.style.display = "block";
-    for (let index = 1; index <= number; index++) {
-        fetchPokemon(index);
-        
+    function fetchPokemons(offset,limit){
+        spinner.style.display = "block";
+        for (let index = offset; index <= offset + limit; index++) {
+        fetchPokemon(index);   
+     }
     }
-}
+
+
+// funcion para llamar a los poquemons por su numero de ID
+//  function fetchPokemons(number){
+    //motramos el spinner al momento de cambiar el grupo de pokemon de pagina
+//    spinner.style.display = "block";
+//    for (let index = 1; index <= number; index++) {
+//        fetchPokemon(index);   
+//    }
+//}
+
 // fetchPokemons(9); "prueba de imprimir en mi consola los datos de mis primeros 9 pokemon"
 
 //Pasamos los datos de la consola a nuestra pagina
@@ -32,7 +61,8 @@ function createPokemon(pokemon){
     spriteContainer.classList.add("img_container");
   
     const sprite = document.createElement("img")
-    sprite.src = pokemon.sprites.front_default
+    sprite.src = pokemon.sprites.other.home.front_default;
+    //sprite.src = pokemon.sprites.front_shiny
   
         spriteContainer.appendChild(sprite);   
   
@@ -50,5 +80,12 @@ function createPokemon(pokemon){
         pokemonContainer.appendChild(card);
   
   }
+
+  function removeChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
   
-  fetchPokemons(9);
+  // fetchPokemons(9); en esta funcion solo llamaba al numero de pokemon que yo quisiera
+  fetchPokemons(offset,limit);
